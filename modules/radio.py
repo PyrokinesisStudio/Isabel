93071166006669903faa0267273a7e0de05452a6
Haruki N. Grande <jordan99oliver@outlook.com>
2017-12-13 20:24:50 +0000

    Update radio.py

--- modules/radio.py
+++ modules/radio.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import discord
 import asyncio
 import sharedstream
-from modules.utils.sharedstream import *
+from modules.utils.sharedstream import * as sharedstream
 import aiohttp
 import os
 import psutil
